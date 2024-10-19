2265636b5d476d79b64317dbfa18b35a2cdaa619
Emmanuel-Peters <stephen.125.peters@gmail.com>
2024-10-19 17:51:39 +0100

    have to figure out the bug

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('script engaged');
+
+let main_container = document.querySelector('.container');
+
+let test_text = document.createElement('p');
+
+
+test_text.textContent = 'JS working!';
+
+main_container.appendChild(test_text);
+
